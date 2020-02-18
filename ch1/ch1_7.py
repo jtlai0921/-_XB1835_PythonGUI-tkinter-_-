efce7387c1137cbc78706e336b9513d1fcdbb60e
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-02-19 00:23:47 +0800

    Initial commit

---
+++ ch1/ch1_7.py
@@ -0,0 +1,15 @@
+# ch1_7.py
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+w = 300     # 視窗寬
+h = 160     # 視窗高
+x = 400     # 視窗左上角x軸位置
+y = 200     # 視窗左上角Y軸位置
+root.geometry("%dx%d+%d+%d" % (w,h,x,y))
+root.mainloop()
+
+
+
+
